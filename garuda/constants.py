ef6e1e6f86f51b9791d6a582608a48a5015d1421
Priyanshu Jain <priyanshu@pm.me>
2019-06-30 13:15:05 +0530

    Update garuda

--- garuda/constants.py
+++ garuda/constants.py
@@ -12,12 +12,8 @@ GARUDA_PORT = default('GARUDA_PORT', '50051')
 GARUDA_SUFFIX = default('GARUDA_SUFFIX', 'Garuda')
 GARUDA_CUSTOM = default('GARUDA_CUSTOM', 'garuda_custom')
 GARUDA_AUTO_PACKAGE = default('GARUDA_AUTO_PACKAGE', 'auto_garuda')
-GARUDA_PROTO_FILE = default('GARUDA_PROTO_FILE', 'garuda.proto')
 
 # Derived constants
-GARUDA_GRPC_FILE = GARUDA_PROTO_FILE.replace('.proto', '_pb2_grpc.py')
-GARUDA_PROTO_PATH = f'{GARUDA_DIR}/{GARUDA_PROTO_FILE}'
-GARUDA_GRPC_PATH = f'{GARUDA_DIR}/{GARUDA_GRPC_FILE}'
 GARUDA_AUTO_MODULE = f'{GARUDA_DIR}.{GARUDA_AUTO_PACKAGE}'
 GARUDA_AUTO_FILE = f'{GARUDA_DIR}/{GARUDA_AUTO_PACKAGE}.py'
 
@@ -28,21 +24,6 @@ GARUDA_IGNORE_FIELDS = default(
 # Fields to remove while generating model
 GARUDA_REMOVE_FIELDS = default('GARUDA_REMOVE_FIELDS', [])
 
-# Dict to translate Django fields into Protobuf fields
-GARUDA_FIELDS = default('GARUDA_FIELDS', dict(
-    CharField='string',
-    DateTimeField='string',
-    BooleanField='bool',
-    EmailField='string',
-    UUIDField='string',
-    ManyToManyField='repeated string',
-    ManyToManyRel='repeated string',
-    TextField='string',
-    PositiveSmallIntegerField='int32',
-    IntegerField='int64',
-    ForeignKey='int64',
-    AutoField='int64',
-))
 
 # `GARUDA_AST_MAP` basically says how to extract data
 # from a given AST `Assign` object
@@ -54,23 +35,6 @@ GARUDA_AST_MAP = default('GARUDA_AST_MAP', dict(
     Attribute=lambda kw: '%s.%s' % (kw.value.value.id, kw.value.attr),
 ))
 
-GARUDA_PROTO_HEADER = default('GARUDA_PROTO_HEADER', '''
-syntax = "proto3";
-
-option java_multiple_files = true;
-option java_package = "com.dhilipsiva.garuda";
-option java_outer_classname = "GarudaProto";
-option objc_class_prefix = "DSG";
-
-package garuda;
-
-message Void {}
-
-message ID {
-    int64 id = 1;
-}
-''')
-GARUDA_PROTO_FOOTER = default('GARUDA_PROTO_FOOTER', '')
 
 GARUDA_CRUD_TEMPLATE = default('GARUDA_CRUD_TEMPLATE', '''
 from %(app)s.models import %(model_name)s  # NOQA
@@ -83,7 +47,7 @@ def read_%(model_name_lower)s(*args, **kwargs):
     except %(model_name)s.DoesNotExist:
         return None
 
-
+true
 def read_%(model_name_lower_plural)s_filter(*args, **kwargs):
     return %(model_name)s.objects.filter(*args, **kwargs)
 
@@ -110,68 +74,4 @@ def update_%(model_name_lower)s(id, *args, **kwargs):
 
 def delete_%(model_name_lower)s(id):
     return %(model_name)s.objects.get(id=id).delete()
-''')
-
-GARUDA_RPC_METHODS = default('GARUDA_RPC_METHODS', '''
-from %(GARUDA_DIR)s.garuda_pb2 import %(model_name)s, Void  # NOQA
-from %(GARUDA_DIR)s.%(app_slug)s_crud import (  # NOQA
-    read_%(model_name_lower)s,
-    delete_%(model_name_lower)s,
-    create_%(model_name_lower)s,
-    update_%(model_name_lower)s,
-    read_%(model_name_lower_plural)s_filter,
-)
-
-
-def %(model_name_lower)s_to_dict(obj):
-    # Cycle through fields directly
-    d = {  }
-    if obj is None:
-        return d
-    is_dj_obj = obj.__module__.endswith('models')
-    foriegn_keys = %(foriegn_keys)s
-    for field in %(fields)s:  # NOQA
-        value = getattr(obj, field, None)
-        if field in [None, 'None']:
-            continue
-        d[field] = value
-        if is_dj_obj and (field == 'id' or field in foriegn_keys):
-            d[field] = str(value)
-        elif is_dj_obj and field in ['created_on', 'updated_on']:
-            d[field] = value.isoformat()
-    return d
-
-
-class %(rpc_name)s:
-
-    def Read%(model_name_plural)sFilter(self, void, context):
-        objs = read_%(model_name_lower_plural)s_filter()
-        return [%(model_name)s(
-            **%(model_name_lower)s_to_dict(obj)) for obj in objs]
-
-    def Read%(model_name)s(self, id, context):
-        obj = read_%(model_name_lower)s(id=id.id)
-        return %(model_name)s(**%(model_name_lower)s_to_dict(obj))
-
-    def Create%(model_name)s(self, obj, context):
-        obj = create_%(model_name_lower)s(**%(model_name_lower)s_to_dict(obj))
-        return %(model_name)s(**%(model_name_lower)s_to_dict(obj))
-
-    def Update%(model_name)s(self, obj, context):
-        obj_dict = %(model_name_lower)s_to_dict(obj)
-        del obj_dict['id']
-        obj = update_%(model_name_lower)s(obj.id, **obj_dict)
-        return Void()
-
-    def Delete%(model_name)s(self, id, context):
-        delete_%(model_name_lower)s(id.id)
-        return Void()
-''')
-
-GARUDA_RPC_CONTENT = default('GARUDA_RPC_CONTENT', '''
-  rpc Delete%(model_name)s(ID) returns (Void);
-  rpc Update%(model_name)s(%(model_name)s) returns (Void);
-  rpc Read%(model_name)s(ID) returns (%(model_name)s);
-  rpc Create%(model_name)s(%(model_name)s) returns (%(model_name)s);
-  rpc Read%(model_name_plural)sFilter(Void) returns (stream %(model_name)s);
 ''')
--- garuda/management/commands/garuda.py
+++ garuda/management/commands/garuda.py
@@ -7,17 +7,15 @@ from importlib import import_module
 from pkg_resources import resource_filename
 
 from inflect import engine
-from grpc_tools import protoc
 from orm_choices.core import user_attributes
 
 from django.apps import apps
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from garuda.constants import GARUDA_SUFFIX, GARUDA_FIELDS, GARUDA_AST_MAP, \
-    GARUDA_IGNORE_FIELDS, GARUDA_RPC_METHODS, GARUDA_CRUD_TEMPLATE, \
-    GARUDA_RPC_CONTENT, GARUDA_REMOVE_FIELDS, GARUDA_DIR, GARUDA_GRPC_PATH, \
-    GARUDA_PROTO_HEADER, GARUDA_PROTO_FOOTER, GARUDA_PROTO_PATH, \
+from garuda.constants import GARUDA_SUFFIX, GARUDA_AST_MAP, \
+    GARUDA_IGNORE_FIELDS, GARUDA_CRUD_TEMPLATE, \
+    GARUDA_REMOVE_FIELDS, GARUDA_DIR, \
     GARUDA_AUTO_FILE, GARUDA_CUSTOM
 
 plural = engine().plural
@@ -67,7 +65,6 @@ def get_inflections(model_name):
         model_name_lower=model_name.lower(),
         model_name_plural=plural(model_name),
         model_name_lower_plural=plural(model_name.lower()),
-        rpc_name=model_name + GARUDA_SUFFIX,
     )
 
 
@@ -119,15 +116,6 @@ def process_apps():
     return app_models
 
 
-def get_rpc_type(field, fields, choices, many_fields):
-    if field in many_fields:
-        return 'repeated string'  # ManyToOneRel fields
-    if field in choices:
-        return choices[field].split(".")[0]
-    dj_field_type = fields[field]
-    return GARUDA_FIELDS[dj_field_type]
-
-
 def generate_model(model_name, model):
     fields = model['fields']
     choices = model['choices']
@@ -140,7 +128,6 @@ def generate_model(model_name, model):
     if 'id' in field_names:
         field_names.remove('id')
     for idx, field in enumerate(sorted(field_names)):
-        field_type = get_rpc_type(field, fields, choices, many_fields)
         fields_declaration += f'\n    {field_type} {field} = {idx + 2};'
     defnition = '\nmessage %s {\n%s\n}\n' % (
         model_name, fields_declaration)
@@ -152,35 +139,6 @@ def generate_app(app, models):
     for model_name, model in models.items():
         app_defnition += generate_model(model_name, model)
     return app_defnition
-
-
-def generate_model_proto(app_models):
-    model_protos = ''
-    for app, models in app_models.items():
-        model_protos += generate_app(app, models)
-    return model_protos
-
-
-def generate_choices_proto():
-    choices_proto = ''
-    for attr in dir(CHOICES):
-        choices = getattr(CHOICES, attr)
-        if not hasattr(choices, 'CHOICES'):
-            continue
-        choices_declaration = f'\n    {choices.__name__}UNKNOWN = 0;'
-        attrs = user_attributes(choices.Meta)
-        if 'UNKNOWN' in attrs:
-            attrs.remove('UNKNOWN')
-        c_dict = {}
-        for attr in attrs:
-            value = getattr(choices.Meta, attr)[0]
-            c_dict.update({attr: value})
-        c_list = sorted(c_dict.items(), key=lambda x: x[1])
-        for key, value in c_list:
-            choices_declaration += f'\n    {key} = {value};'
-        choices_proto += '\nenum %s {%s\n}\n' % (
-            choices.__name__, choices_declaration)
-    return choices_proto
 
 
 def sluggify(app):
@@ -210,106 +168,26 @@ def codify_model(app, model_name, model_defnition):
         app=app, app_slug=sluggify(app), fields=fields, GARUDA_DIR=GARUDA_DIR,
         foriegn_keys=foriegn_keys, ignore_fields=GARUDA_IGNORE_FIELDS)
     ctx.update(model_defnition['inflections'])
-    return GARUDA_CRUD_TEMPLATE % ctx, GARUDA_RPC_METHODS % ctx
+    return GARUDA_CRUD_TEMPLATE % ctx
 
 
 def codify_app(app, models):
     crud_contents = ''
-    rpc_contents = ''
     for model in models.keys():
-        crud_content, rpc_content = codify_model(app, model, models[model])
+        crud_content = codify_model(app, model, models[model])
         crud_contents += crud_content
-        rpc_contents += rpc_content
-    write_to_file(app, 'rpc', rpc_contents)
     write_to_file(app, 'crud', crud_contents)
 
 
-def generate_rpc_code(app_models):
+def generate_code(app_models):
     for app in app_models.keys():
         codify_app(app, app_models[app])
 
 
-def generate_garuda_rpc(app_models):
-    content = ''
-    for app in app_models:
-        models = app_models[app]
-        for model in models:
-            content += GARUDA_RPC_CONTENT % models[model]['inflections']
-    try:
-        garuda_custom = import_module(GARUDA_CUSTOM)
-    except ImportError:
-        print(f'No Custom RPC declaration in {app}')
-        return content
-    if not hasattr(garuda_custom, 'GarudaCustom'):
-        print(f'GarudaCustom class not found in {GARUDA_CUSTOM} package')
-        return content
-    for attribute in user_attributes(garuda_custom.GarudaCustom):
-        content += '  %s\n' % getattr(
-            garuda_custom.GarudaCustom, attribute).__doc__
-    return content
-
-
-def generate_auto_garuda(app_models):
-    content = ''
-    models = []
-    for app in app_models:
-        _models = [
-            f'%s%s' % (model, GARUDA_SUFFIX)
-            for model in app_models[app].keys()]
-        models += _models
-        _models = ", ".join(_models)
-        app = sluggify(app)
-        content += f'\nfrom {GARUDA_DIR}.{app}_rpc import {_models}  # NOQA'
-    models = ", ".join(models)
-    content += f'\n\nclass AutoGaruda({models}):  # NOQA\n    pass'
-    with open(GARUDA_AUTO_FILE, "w") as f:
-        print(f'writing to {f.name}')
-        f.write(content)
-
-
-def protoc_arguments():
-    '''
-    Construct protobuf compiler arguments
-    '''
-    proto_include = resource_filename('grpc_tools', '_proto')
-    return [
-        protoc.__file__, '-I', GARUDA_DIR, f'--python_out={GARUDA_DIR}',
-        f'--grpc_python_out={GARUDA_DIR}', GARUDA_PROTO_PATH,
-        f'-I{proto_include}']
-
-
-def fix_grpc_import():
-    '''
-    Snippet to fix the gRPC import path
-    '''
-    with open(GARUDA_GRPC_PATH, 'r') as f:
-        filedata = f.read()
-    filedata = filedata.replace(
-        'import garuda_pb2 as garuda__pb2',
-        f'import {GARUDA_DIR}.garuda_pb2 as garuda__pb2')
-    with open(GARUDA_GRPC_PATH, 'w') as f:
-        f.write(filedata)
-
-
 class Command(BaseCommand):
-    help = 'Generate proto files'
+    help = 'Generate crud files'
 
     def handle(self, *args, **options):
         ensure_data()
         app_models = process_apps()
-        generate_rpc_code(app_models)
-        generate_auto_garuda(app_models)
-        CHOICES_PROTO = generate_choices_proto()
-        MODELS_PROTO = generate_model_proto(app_models)
-        GARUDA_RPC = "service Garuda{%s}" % generate_garuda_rpc(app_models)
-        with open(f'{GARUDA_PROTO_PATH}', 'w') as f:
-            print(f'writing to {f.name} ...')
-            f.write(f'''
-{GARUDA_PROTO_HEADER}
-{CHOICES_PROTO}
-{MODELS_PROTO}
-{GARUDA_RPC}
-{GARUDA_PROTO_FOOTER}
-                    '''.strip())
-        protoc.main(protoc_arguments())
-        fix_grpc_import()
+        generate_code(app_models)
